7c3bdceffd4a5359d65f9ef0b8bd2973522875ef
Dushyanth Yadav v <dushyanthyadav6@gmail.com>
2025-04-18 15:58:26 +0530

    Remove target folder from repository and ignore it

--- To_do_app/target/debug/deps/To_do_app-a50191e3a6457b56.d
+++
@@ -1,7 +0,0 @@
-/home/rust/To_do_app/target/debug/deps/libTo_do_app-a50191e3a6457b56.rmeta: src/main.rs src/to_do_list.rs src/helper_function.rs
-
-/home/rust/To_do_app/target/debug/deps/To_do_app-a50191e3a6457b56.d: src/main.rs src/to_do_list.rs src/helper_function.rs
-
-src/main.rs:
-src/to_do_list.rs:
-src/helper_function.rs:
--- To_do_app/target/debug/deps/To_do_app-f3ed44aa9234de5e.d
+++
@@ -1,7 +0,0 @@
-/home/rust/To_do_app/target/debug/deps/To_do_app-f3ed44aa9234de5e: src/main.rs src/to_do_list.rs src/helper_function.rs
-
-/home/rust/To_do_app/target/debug/deps/To_do_app-f3ed44aa9234de5e.d: src/main.rs src/to_do_list.rs src/helper_function.rs
-
-src/main.rs:
-src/to_do_list.rs:
-src/helper_function.rs:
